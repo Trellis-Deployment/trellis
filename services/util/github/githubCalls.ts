a4d8ad66f793484f1973a70be72db7cc55d81613
Mohamad EL-Chanti <m.elchanti@gmail.com>
2022-10-25 13:11:11 -0400

    added githubCalls

---
+++ services/util/github/githubCalls.ts
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { Octokit } from "octokit";
+
+
+
+const getAccessToken = async (code) => {
+  try {
+    const response = await axios.post(`https://github.com/login/oauth/access_token?client_id=${process.env.Client_ID}&client_secret=${process.env.Client_secret}&code=${code}`);
+    return response.data;
+  } catch(e) {
+    console.log(e.message);
+  }
+}
+
+const getUserInfo = async (token) => {
+  const octokit = new Octokit({
+    auth: token
+  });
+  try {
+    const response = await octokit.request('Get /user', {});
+    return response.data;
+  } catch(e) {
+    console.log(e.message);
+  }
+}
+
+const getReposByToken = async (token) => {
+  const octokit = new Octokit({
+    auth: token
+  });
+
+  const repos: object[] = [];
+  try {
+    const installations = await octokit.request('GET /user/installations', {});
+    for (const installation of installations.data.installations) {
+      const installation_id = installation.id;
+      const installationRepos = await octokit.request(`GET /user/installations/${installation_id}/repositories`, {
+        installation_id
+      });
+      installationRepos.data.repositories.forEach(repo => {
+        repos.push(repo);
+      });
+    }
+    return repos;
+  } catch(e) {
+    console.log(e.message);
+  }
+}
+
+const createWebhook = async (token, webhookURL, user, fullRepo) => {
+  const octokit = new Octokit({
+    auth: token
+  });
+  let [owner, repo] = fullRepo.split("/");
+  
+  try {
+    const response = await octokit.request('POST /repos/{owner}/{repo}/hooks', {
+      owner: owner,
+      repo: repo,
+      name: 'web',
+      active: true,
+      events: [
+        'push'
+      ],
+      config: {
+        url: webhookURL,
+        content_type: 'json',
+        insecure_ssl: '0'
+      }
+    });
+  } catch(e) {
+    console.log({githubFailed: e.message});
+    throw new Error(`{githubFailed: ${e.message}}`);
+  }
+}
+const githubCalls = {
+  getAccessToken,
+  getUserInfo,
+  getReposByToken,
+  createWebhook
+};
+
+export default githubCalls;
