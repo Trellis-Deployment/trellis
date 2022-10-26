af0d2083cd234e2ad6f1a0aeab3fa3bf499b37c6
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-10-26 11:30:48 -0400

    UI Improvements

--- client/src/components/Applications.js
+++ client/src/components/Applications.js
@@ -1,100 +1,149 @@
-import "../stylesheets/Applications.css";
+import "../stylesheets/AppModal.css";
 import Button from "react-bootstrap/Button";
-import { useState, useEffect } from "react";
-import APICalls from "../services/APICalls";
-import { useNavigate } from "react-router-dom";
-import Container from "react-bootstrap/Container";
+import MiniNavBar from "./Header/MiniNavBar";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import APICalls from "../services/APICalls";
+import { CloudCheck, Check2Circle, Code } from "react-bootstrap-icons";
 
-const Applications = ({ authUser }) => {
-  const [applications, setApplications] = useState([]);
-  const navigate = useNavigate();
+const AppModal = ({ authUser }) => {
+  const [stages, setStages] = useState([]);
+  const appName = useParams().appName;
+  console.log(useParams());
 
   useEffect(() => {
-    const getApps = async () => {
-      try {
-        const data = await APICalls.getApps(authUser);
-        setApplications(data);
-      } catch (e) {
-        console.log(e.message);
-      }
+    const getStages = async () => {
+      const data = await APICalls.getStages(authUser, appName);
+      setStages(data);
     };
+    getStages();
+  }, [authUser, appName]);
 
-    getApps();
-  }, [authUser]);
-
-  const handleNewAppClick = (e) => {
+  const handleDeployClick = async (e, stageName) => {
     e.preventDefault();
-    navigate("/create-app");
+    const data = await APICalls.buildStage({ authUser, appName, stageName });
+    console.log(data);
   };
 
-  const handleAppClick = (e, appName) => {
-    e.preventDefault();
-    navigate(`/application/${appName}`);
-  };
 
   return (
-    <div className="app-list-home" class="container">
-      <Container class="pt-1 px-1 pb-3">
-        <Row>
-          <Col class="start-0">
-            <h3 class="text-start ps-3">Welcome {authUser}</h3>
-          </Col>
-          <Col class="#">
-            <Button
-              onClick={handleNewAppClick}
-              variant="success"
-              size="sm"
-              class="#"
-            >
-              &#43; New App
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+    <div>
+      <div class="px-3">
+        <MiniNavBar></MiniNavBar>
+      </div>
 
-      <ul class="container">
-        {applications.map((application) => (
-          <div class="row">
-            <li className="m-1">
-              <Container class="bg-dark">
-                <Card
-                  key={application.appId}
-                  onClick={(e) => handleAppClick(e, application.appName)}
-                  className="card-face w-auto"
+      <div class="container">
+        <div class="card p-2 pipes mt-3 mid-wide-card">
+          <div class="container">
+            <div class="row">
+              <div class="row">
+                <div
+                  class="col 
+                pipeline-title"
                 >
-                  <Row>
-                    <Col>
-                      <Card.Body class="ps-3 py-2">
-                        <Card.Title class="text-start c-title">
-                          App: {application.appName}
-                        </Card.Title>
-                        <Card.Subtitle class="text-start c-subtitle">
-                          Repo: {application.repo}
-                        </Card.Subtitle>
-                      </Card.Body>
+                  Pipeline
+                  <a class="ps-2 small-font" href="/apps">
+                    edit
+                  </a>
+                </div>
+                <div
+                  class="d-flex col pe-0
+                justify-content-end 
+                align-items-end
+               small-font"
+                >
+                  <a href="/apps">View Full Pipeline</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="row border">
+              <div class="col  bg-white">
+                <div class="row ">
+                  {stages.map((stage) => (
+                    <Col key={stage.stageId} className="stage-row" class="#">
+                      <Card.Title class="SectionHeader">
+                        Stage Name: {stage.stageName}
+                      </Card.Title>
+                      <Card.Text class="stage-branch">
+                        <Row>
+                          Stage Branch:{" "}
+                          {stage.stageBranch !== "undefined"
+                            ? stage.stageBranch
+                            : "no branch"}{" "}
+                          <Col class="lh-0">
+                            {stage.stageBranch === "main" ? (
+                              <div>
+                                <p class="line-2">
+                                  <a
+                                    target="_blank"
+                                    class="branch px-1"
+                                    rel="noopener noreferrer"
+                                    href="/"
+                                  >
+                                    <i
+                                      aria-hidden="true"
+                                      class="fa  fa-code-fork "
+                                    ></i>
+                                    main
+                                  </a>
+                                  <a
+                                    target="_blank"
+                                    class="commit ps-1"
+                                    rel="noopener noreferrer"
+                                    href="https://github.com/Maru-ko/notes-one1/commit/abd29d1df4dcb5cd3b8a0a141b9276c0ba6b7029"
+                                  >
+                                    abd29d1
+                                  </a>
+                                </p>
+                                <Col>
+                                  <CloudCheck color="green" size={28} />
+                                </Col>
+                              </div>
+                            ) : (
+                              <div>
+                                <p>-</p>{" "}
+                                <Col>
+                                  {" "}
+                                  <Code color="grey" size={28} />
+                                </Col>
+                              </div>
+                            )}
+                          </Col>
+                        </Row>
+                      </Card.Text>
+
+                      <Row>
+                        <button
+                          class="btn btn-sm px-1"
+                          type="button"
+                          onClick={(e) => handleDeployClick(e, stage.stageName)}
+                        >
+                          <text>Manually Deploy Stage</text>
+                        </button>
+                      </Row>
                     </Col>
-                    {/* <Col class="#">
-              <span class="badge rounded-pill bg-danger">
-                {application.appName[0].toUpperCase()}
-              </span>
-            </Col> */}
-                    {/* <Col></Col> */}
-                  </Row>
-                </Card>
-                {/* </Col> */}
-                {/* </Row> */}
-              </Container>
-            </li>
+                  ))}
+                </div>
+                <div class="card mini-card">
+                  <row class="stage-title"></row>
+                </div>
+              </div>
+            </div>
           </div>
-        ))}
-      </ul>
+        </div>
+      </div>
+
+      <div class="container py-3">
+      </div>
+
+ 
     </div>
   );
 };
 
-
-
-export default Applications;
+export default AppModal;
