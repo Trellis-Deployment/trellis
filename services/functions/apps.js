ce2a1cef887a01c6b7bf6dee0dbe391138db035c
Mohamad EL-Chanti <m.elchanti@gmail.com>
2022-10-27 21:00:53 -0400

    edited manualBuild

--- services/functions/apps.js
+++ services/functions/apps.js
@@ -4,6 +4,7 @@ import getAppsByOwnerId from "../util/appsTableUtils/getAppsByOwnerId";
 
 export const main = handler(async (event) => {
   const login = await event['queryStringParameters']['user'];
+
   const apps = await getAppsByOwnerId(login);
   return(apps);
 });
