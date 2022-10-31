7b99ac6ce85b71d8c878190f7dbcc14a02ae2465
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-10-30 22:01:00 -0500

    Remove conditional requires stage branch to be "main" in order to render

--- client/src/components/AppMainPage/StageData.js
+++ client/src/components/AppMainPage/StageData.js
@@ -5,62 +5,65 @@ import {
   CloudCheck,
   Code,
   ExclamationCircle,
-  Gear
+  Gear,
 } from "react-bootstrap-icons";
 
-const stages = ({ stage }) => {
-
-  if (stage.stageBranch === 'main') {
-    return (
-      <div>
-        <Row>
-          {" "}
-          <div className="line-2">
+const StageData = ({ stage }) => {
+  return (
+    <div className="stage-info">
+      <Row>
+        {" "}
+        <div className="line-2">
+          <a
+            target="_blank"
+            className="branch px-1"
+            rel="noopener noreferrer"
+            href="/"
+          >
+            <i aria-hidden="true" className="fa  fa-code-fork "></i>
+            main
+          </a>
+          <Row>
             <a
               target="_blank"
-              className="branch px-1"
+              className="commit ps-1 stage-info"
               rel="noopener noreferrer"
-              href="/"
+              href={stage.stageId}
             >
-              <i
-                aria-hidden="true"
-                className="fa  fa-code-fork "
-              ></i>
-              main
+              Last Deployed: {String(new Date(stage.lastDeploymentTime))}
             </a>
-            <Row>
-              <a
-                target="_blank"
-                className="commit ps-1"
-                rel="noopener noreferrer"
-                href={stage.stageId}
-              >
-                Last Deployed: {String(new Date(stage.lastDeploymentTime))}
-              </a>
-              App ID: {stage.appId.split('-')[0]}
-            </Row>
-          </div>
-        </Row>
-        {stage.stageStage ==='created' && <Col className="text-center"><CloudCheck color="yellow" size={28}/></Col>}
-        {stage.stageState === 'deployed' && <Col className="text-center"> <CloudCheck color="green" size={28}/></Col>}
-        {stage.stageState === 'deploying' &&  <Col><Gear className="text-center text-light spinner-border"color="dark" size={29} /></Col>}
-        {stage.stageState === 'error' && <Col className="text-center"> <ExclamationCircle color="red" size={28} /></Col>}
-      </div>
-    ) 
-  } else {
-    return (
-      <div>
-        <Col>
-          <Code color="grey" size={28} />
+            App ID: {stage.appId.split("-")[0]}
+          </Row>
+        </div>
+      </Row>
+      {stage.stageStage === "created" && (
+        <Col className="text-center">
+          <CloudCheck color="yellow" size={28} />
         </Col>
-      </div>
-    )
+      )}
+      {stage.stageState === "deployed" && (
+        <Col className="text-center">
+          {" "}
+          <CloudCheck color="green" size={28} />
+        </Col>
+      )}
+      {stage.stageState === "deploying" && (
+        <Col>
+          <Gear
+            className="text-center text-light spinner-border"
+            color="dark"
+            size={29}
+          />
+        </Col>
+      )}
+      {stage.stageState === "error" && (
+        <Col className="text-center">
+          {" "}
+          <ExclamationCircle color="red" size={28} />
+        </Col>
+      )}
+    </div>
+  );
+};
 
-  }
-}
-
-export default stages;
-
-
-
-
+export default StageData;
