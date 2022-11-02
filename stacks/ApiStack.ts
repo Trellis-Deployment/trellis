7d01029668f8cc0a81536479bc900bd2e8812aaf
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-11-01 20:58:18 -0500

    Add storeAWSCredentials lambda to default function environment

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,10 +1,17 @@
-import { Api, use, Function, StackContext, WebSocketApi } from "@serverless-stack/resources";
+import {
+  Api,
+  use,
+  Function,
+  StackContext,
+  WebSocketApi,
+} from "@serverless-stack/resources";
 import { StorageStack } from "./StorageStack";
 import { BuildServerStack } from "./BuildServerStack";
 import config from "../util/config";
 
 export function ApiStack({ stack, app }: StackContext) {
-  const { users, apps, stages, deployments } = use(StorageStack);
+  const { users, apps, stages, deployments, storeAWSCredentialsLambda } =
+    use(StorageStack);
   const { vpc, buildFunction } = use(BuildServerStack);
 
   const Client_ID = config.Client_ID ? config.Client_ID : "undefined";
@@ -13,12 +20,12 @@ export function ApiStack({ stack, app }: StackContext) {
   const webSocketMessage = new Function(stack, "WebSocketFunction", {
     handler: "websocket/sendMessage.handler",
     permissions: [users, apps, stages, deployments],
-      environment: {
-        USERS_TABLE_NAME: users.tableName,
-        APPS_TABLE_NAME: apps.tableName,
-        STAGES_TABLE_NAME: stages.tableName,
-        DEPLOYMENTS_TABLE_NAME: deployments.tableName,
-      },
+    environment: {
+      USERS_TABLE_NAME: users.tableName,
+      APPS_TABLE_NAME: apps.tableName,
+      STAGES_TABLE_NAME: stages.tableName,
+      DEPLOYMENTS_TABLE_NAME: deployments.tableName,
+    },
   });
 
   // Create the WebSocket API
@@ -34,7 +41,7 @@ export function ApiStack({ stack, app }: StackContext) {
             DEPLOYMENTS_TABLE_NAME: deployments.tableName,
           },
           handler: "websocket/connect.handler",
-        }
+        },
       },
       $disconnect: {
         function: {
@@ -46,11 +53,11 @@ export function ApiStack({ stack, app }: StackContext) {
             DEPLOYMENTS_TABLE_NAME: deployments.tableName,
           },
           handler: "websocket/disconnect.handler",
-        }
+        },
       },
       sendMessage: {
         function: webSocketMessage,
-      }
+      },
     },
     accessLog: false,
   });
@@ -58,7 +65,15 @@ export function ApiStack({ stack, app }: StackContext) {
   const api = new Api(stack, "Api", {
     defaults: {
       function: {
-        permissions: [users, apps, stages, deployments, buildFunction, webSocketMessage],
+        permissions: [
+          users,
+          apps,
+          stages,
+          deployments,
+          buildFunction,
+          webSocketMessage,
+          storeAWSCredentialsLambda,
+        ],
         environment: {
           USERS_TABLE_NAME: users.tableName,
           APPS_TABLE_NAME: apps.tableName,
@@ -69,6 +84,8 @@ export function ApiStack({ stack, app }: StackContext) {
           DEPLOY_LAMBDA_NAME: buildFunction.functionName,
           WEB_SOCKET_LAMBDA_NAME: webSocketMessage.functionName,
           WEB_SOCKET_URL: webSocketApi.url,
+          STORE_AWS_CREDENTIALS_LAMBDA_NAME:
+            storeAWSCredentialsLambda.functionName,
         },
       },
     },
@@ -99,7 +116,6 @@ export function ApiStack({ stack, app }: StackContext) {
 
   return {
     api,
-    webSocketApi
+    webSocketApi,
   };
 }
-
