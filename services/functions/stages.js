868c7a6465cf3cb9f430f51f176f222881439783
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-11-01 21:13:59 -0500

    Remove IAMCredentialLocation property from stages API lambda

--- services/functions/stages.js
+++ services/functions/stages.js
@@ -1,14 +1,17 @@
 import handler from "../util/templates/handler";
 import getStagesByAppId from "../util/stagesTableUtils/getStagesByAppId";
 
-
 export const main = handler(async (event) => {
-  const appId = await event['queryStringParameters']['appId'];
+  const appId = await event["queryStringParameters"]["appId"];
   try {
     const stages = await getStagesByAppId(appId);
-    return(stages);
-  } catch(e) {
+    stages.forEach((stage) => {
+      delete stage.IAMCredentialsLocation;
+    });
+    return stages;
+  } catch (e) {
     console.log(e.message);
-    return(e.message);
+    return e.message;
   }
 });
+
