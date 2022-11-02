384dd8dc8f059fe0903f8df61881910435805cb9
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-11-01 21:05:55 -0500

    Update deployment helpers to remove plaintext IAM credentials

--- services/util/deployment/getDataForManualDeployment.js
+++ services/util/deployment/getDataForManualDeployment.js
@@ -2,14 +2,22 @@ import getAppByUserAndAppName from "../appsTableUtils/getAppByUserAndAppName";
 import getTokenByUserId from "../usersTableUtils/getTokenByUserId";
 import getStageById from "../stagesTableUtils/getStageById";
 
-const getDataForManualDeployment = async ({userId, appName, stageId }) => {
-  const app = (await getAppByUserAndAppName({userId, appName}));
-  const appId  = app.appId;
+const getDataForManualDeployment = async ({ userId, appName, stageId }) => {
+  const app = await getAppByUserAndAppName({ userId, appName });
+  const appId = app.appId;
   const stage = await getStageById(stageId);
-  console.log({stage});
+  console.log({ stage });
   const token = await getTokenByUserId(userId);
   const [user, repoName] = app.repoName.split("/");
-  return { stage, appName, stageName: stage.stageName, token, user, repoName, IAMAccessKey: stage.IAMAccessKey, IAMSecretKey: stage.IAMSecretKey };
+  return {
+    stage,
+    appName,
+    stageName: stage.stageName,
+    token,
+    user,
+    repoName,
+    IAMCredentialsLocation: stage.IAMCredentialsLocation,
+  };
 };
 
 export default getDataForManualDeployment;
