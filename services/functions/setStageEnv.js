48e35b7957f844c5ea825fecc209c884bc8785a2
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-11-03 16:13:21 -0500

    Add backend lambdas for storing/updating environment secrets

---
+++ services/functions/setStageEnv.js
@@ -0,0 +1,40 @@
+import handler from "../util/templates/handler";
+import setStageEnvLocation from "../util/stagesTableUtils/setStageEnvLocation";
+import getStageById from "util/stagesTableUtils/getStageById";
+import AWS from "aws-sdk";
+
+const REGION = process.env.REGION;
+
+export const main = handler(async (event) => {
+  let { envJSONString, stageId } = JSON.parse(event.body);
+
+  const stage = await getStageById(stageId);
+  let secretName = stage.envLocation;
+  let update = true;
+
+  if (!secretName) {
+    secretName = "trellis/" + uuidv4();
+    update = false;
+  }
+
+  try {
+    const lambda = new AWS.Lambda();
+    const params = {
+      FunctionName: process.env.STORE_ENV_LAMBDA_NAME,
+      InvocationType: "Event",
+      LogType: "Tail",
+      Payload: {
+        region: REGION,
+        secretName,
+        envJSONString,
+        update,
+      },
+    };
+    await lambda.invoke(params).promise();
+    await setStageEnvLocation(stageId, secretName);
+    return "Environment variables updated";
+  } catch (e) {
+    console.log(e.message);
+    return { error: e.message };
+  }
+});
